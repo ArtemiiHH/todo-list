ac79c5b722daa399e38eba4cadb5ef393e54caf3
ArtemiiHH <artemiibarashkov2003@gmail.com>
2025-10-19 21:57:14 +0300

    Find id by index

--- src/modules/project.js
+++ src/modules/project.js
@@ -3,8 +3,20 @@ import { createTodo } from "./todo";
 
 export function createProject(name) {
   const todos = [];
-  const addTodo = (todo) => todos.push(todo);
-  const getTodos = () => todos;
-  const removeTodo = (todo) => todos.pop(todo);
+
+  // Add a todo (already created outside)
+  const addTodo = (todo) => {
+    todos.push(todo);
+  };
+
+  // Return a copy of todos to protect internal state
+  const getTodos = () => [...todos];
+
+  // Remove a specific todo by id or reference
+  const removeTodo = (id) => {
+    const index = todos.findIndex((task) => task.id === id);
+    if (index !== -1) todos.splice(index, 1);
+  };
+
   return { name, addTodo, getTodos, removeTodo };
 }
