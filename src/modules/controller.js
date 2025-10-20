838d0023b1b8b2b10ca335f76e805bdffbfa0b11
ArtemiiHH <artemiibarashkov2003@gmail.com>
2025-10-20 14:41:22 +0300

    Import storage to controller

--- src/modules/controller.js
+++ src/modules/controller.js
@@ -1,12 +1,18 @@
 // Control the flow of the App
 import { createProject } from "./project";
 import { createTodo } from "./todo";
+import { Storage } from "./storage";
 
 // Initialize function
 export function init() {
-  
-  // Create Default project
-  return function defaultProject() {
-    createProject("Inbox");
-  };
+  // Load project from storage
+  Storage.loadProjects(createProject);
+
+  // If projects dont exist, load default
+  if (Storage.loadProjects(!createProject)) {
+    
+    // Create Default project
+    const defaultProject = createProject("Inbox");
+    activeProject = defaultProject;
+  }
 }
