1e20b53d14797bc15505a1657a7bc9646b39be7e
ArtemiiHH <artemiibarashkov2003@gmail.com>
2025-10-20 16:53:08 +0300

    Add active state + null to if statement

--- src/modules/controller.js
+++ src/modules/controller.js
@@ -3,16 +3,21 @@ import { createProject } from "./project";
 import { createTodo } from "./todo";
 import { Storage } from "./storage";
 
+let activeProject;
+
 // Initialize function
 export function init() {
   // Load project from storage
-  Storage.loadProjects(createProject);
+  Storage.loadProjects();
+  let projects = Storage.loadProjects();
 
   // If projects dont exist, load default
-  if (Storage.loadProjects(!createProject)) {
+  if (!projects || projects.length === 0) {
     
     // Create Default project
-    const defaultProject = createProject("Inbox");
-    activeProject = defaultProject;
+    let defaultProject = createProject("Inbox");
+    projects.push(defaultProject);
+    activeProject = projects[0];
+    Storage.loadProjects();
   }
 }
