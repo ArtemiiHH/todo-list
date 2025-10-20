b7aeef3ae24c46cfde7c152971aecf98556f5a75
ArtemiiHH <artemiibarashkov2003@gmail.com>
2025-10-20 13:51:43 +0300

    Add initalize function

--- src/modules/controller.js
+++ src/modules/controller.js
@@ -1,2 +1,11 @@
 // Control the flow of the App
 import { createProject } from "./project";
+import { createTodo } from "./todo";
+
+export function initialize() {
+  
+  // Create Default project
+  return function defaultProject() {
+    createProject("Inbox");
+  };
+}
