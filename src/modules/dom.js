f6950aa7771710a2725c5681a6e405fd81a4b8ab
ArtemiiHH <artemiibarashkov2003@gmail.com>
2025-10-21 13:03:28 +0300

    Add Render, Bind groups with functions

--- src/modules/dom.js
+++ src/modules/dom.js
@@ -1,2 +1,32 @@
 // DOM handler
 import { init } from "./controller";
+import { createProject } from "./project";
+import { createTodo } from "./todo";
+
+// Render functions group
+const Renderer = {
+  renderProjects(projects, activeProjectId) {},
+
+  renderTodos(todos) {},
+
+  renderEmpty() {},
+};
+
+// Bind functions group
+const Binder = {
+  bindAddTodo(handler) {},
+
+  bindEditTodo(handler) {},
+
+  bindDeleteTodo(handler) {},
+
+  bindToggleTodo(handler) {},
+
+  bindSwitchProject(handler) {},
+
+  bindAddProject(handler) {},
+
+  bindDeleteProject(handler) {},
+};
+
+export { Renderer, Binder };
