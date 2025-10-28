4a0ec6a70ec2d1b55dffba043b46a9cdb091c68e
ArtemiiHH <artemiibarashkov2003@gmail.com>
2025-10-28 13:12:01 +0200

    Restructure the whole module

--- src/modules/controller.js
+++ src/modules/controller.js
@@ -1,67 +1,12 @@
-// Control the flow of the App
-import { createProject } from "./project";
-import { createTodo } from "./todo";
-import { Storage } from "./storage";
 import { Renderer, Binder } from "./dom";
 
-let projects = [];
-let activeProject;
-
-// Handlers Object
 const Handlers = {
   handleAddTodo() {
-    // Create todo
-    const newTodo = createTodo();
-    // Add todo to active project
-    activeProject.addTodo(newTodo);
-    // Save projects
-    Storage.saveProjects(projects);
-    // Re-render
-    Renderer.renderTodos(activeProject.getTodos());
+    console.log("Add todo clicked!");
   },
-
-  handleEditTodo() {},
-
-  handleDeleteTodo() {},
-
-  handleAddProject() {},
-
-  handleSwitchProject() {},
-
-  handleDeleteProject() {},
 };
 
-// Initialize function
 export function init() {
-  // Load project from storage
-  projects = Storage.loadProjects();
-
-  // Check for empty or missing data
-  if (!projects || projects.length === 0) {
-    // Make sure projects is an array
-    projects = [];
-
-    // Create Default project
-    let defaultProject = createProject("Inbox");
-    projects.push(defaultProject);
-    activeProject = projects[0];
-
-    // Save back to Storage
-    Storage.saveProjects(projects);
-  }
-
-  // Set active project for first project
-  activeProject = projects[0];
-
-  // Render the initial UI
-  // Render Sidebar
-  Renderer.renderProjects(projects, activeProject);
-  // Render Todos in Content
-  Renderer.renderTodos(activeProject.getTodos());
-
-  // Connect Binder to Hanlders
-  Binder.bindAddTodo(Handlers.handleAddTodo);
-  Binder.bindAddProject(Handlers.handleAddProject);
+    console.log("Init called");
+    Binder.bindAddTodo(Handlers.handleAddTodo);
 }
-
-export { Handlers };
