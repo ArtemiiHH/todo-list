966cbe06ac9aa9d22f64c4ea85c39e61a15c332a
ArtemiiHH <artemiibarashkov2003@gmail.com>
2025-10-28 14:49:43 +0200

    Import Storage + add dummy todo

--- src/modules/controller.js
+++ src/modules/controller.js
@@ -1,20 +1,25 @@
 import { Renderer, Binder } from "./dom";
+import { Storage } from "./storage";
 
 // Handler functions group
 const Handlers = {
   handleAddTodo() {
     console.log("Add todo clicked!");
+
+    const dummyTodo = {
+      title: "Test task",
+      description: "Test description...",
+    };
   },
 
   handleAddProject() {
-    console.log('Add project clicked!');
+    console.log("Add project clicked!");
   },
 };
 
 // Initialize function
 export function init() {
-
   // Bind the handlers to Binders in DOM
-    Binder.bindAddTodo(Handlers.handleAddTodo);
-    Binder.bindAddProject(Handlers.handleAddProject);
+  Binder.bindAddTodo(Handlers.handleAddTodo);
+  Binder.bindAddProject(Handlers.handleAddProject);
 }
