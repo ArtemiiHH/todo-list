080186be0871e6fcf1f15c7f43b226834fcf55e6
ArtemiiHH <artemiibarashkov2003@gmail.com>
2025-11-02 17:06:10 +0200

    Add handleSaveTodo func + bind it to DOm

--- src/modules/controller.js
+++ src/modules/controller.js
@@ -13,7 +13,7 @@ const Handlers = {
   handleAddTodo() {
     console.log("Add todo clicked!");
 
-    const newTodo = createTodo("Real task", "Description...");
+    const newTodo = createTodo(taskTitleInput.value, taskDescInput.value);
 
     todos.push(newTodo);
     Renderer.renderTodos(todos);
@@ -24,6 +24,8 @@ const Handlers = {
   handleAddProject() {
     console.log("Add project clicked!");
   },
+
+  handleSaveTodo() {}
 };
 
 // Initialize function
@@ -31,4 +33,5 @@ export function init() {
   // Bind the handlers to Binders in DOM
   Binder.bindAddTodo(Handlers.handleAddTodo);
   Binder.bindAddProject(Handlers.handleAddProject);
+  Binder.bindSaveTodo(Handlers.handleSaveTodo);
 }
