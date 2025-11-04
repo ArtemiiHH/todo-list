d0c0ff2cb448bab21c22da03aeac0429c5ab256f
ArtemiiHH <artemiibarashkov2003@gmail.com>
2025-11-04 13:59:28 +0200

    Restructure the module

--- src/modules/project.js
+++ src/modules/project.js
@@ -1,22 +1,15 @@
 // Group of TODOs
 import { createTodo } from "./todo";
 
-export function createProject(name) {
+export function createProject(title, id = crypto.randomUUID()) {
   const todos = [];
 
-  // Add a todo (already created outside)
-  const addTodo = (todo) => {
-    todos.push(todo);
-  };
-
-  // Return a copy of todos to protect internal state
+  const addTodo = (todo) => todos.push(todo);
   const getTodos = () => [...todos];
-
-  // Remove a specific todo by id or reference
   const removeTodo = (id) => {
     const index = todos.findIndex((task) => task.id === id);
     if (index !== -1) todos.splice(index, 1);
   };
 
-  return { name, addTodo, getTodos, removeTodo };
+  return { id, title, addTodo, getTodos, removeTodo };
 }
